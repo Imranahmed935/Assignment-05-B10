4f2a501e2d43533c4a8e1199e67ef1988130db9a
Imranahmed935 <imrantahir9918@gmail.com>
2024-09-23 09:29:12 -0700

    first modal done

--- js/index.js
+++ js/index.js
@@ -1,22 +1,52 @@
 const donateBtn = document.getElementById('btn-donate');
 donateBtn.onclick = ()=>{
+    const history = document.getElementById('history');
+    const titleValue = getTitleValue('title1');
     const inputValue = getInputValue('amount-input');
     const mainBalance = getTextValue('balance');
     const collectBalance = getTextValue('collect-balance');
+    const modal = document.getElementById('modal');
+    const closeBtn = document.getElementById('close-btn');
     const donateBalance = mainBalance - inputValue;
     const addBalance = collectBalance + inputValue;
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <h2 class="text-xl mb-2 font-semibold">${inputValue} Taka is ${titleValue}</h2>
+      <p class="text-black">Date: ${new Date().toLocaleDateString()}, time: ${new Date().toLocaleTimeString()}</p>
+    `;
+    div.classList.add('mt-10', 'border', 'p-3' ,'rounded-xl',)
+    history.appendChild(div);
+
+    modal.classList.remove('hidden');
+    closeBtn.onclick = function(){
+    modal.classList.add('hidden')
+   }
     
+
+
     document.getElementById('collect-balance').innerText = addBalance;
     document.getElementById('balance').innerText = donateBalance;
     document.getElementById('amount-input').value = '';
 }
 const donateBtn2 = document.getElementById('donate-btn2');
 donateBtn2.onclick = ()=>{
+    const history = document.getElementById('history');
     const inputValue = getInputValue('amount-input2');
     const mainBalance = getTextValue('balance');
     const collectBalance = getTextValue('card-balance1');
+    const titleValue = getTitleValue('title2');
     const donateBalance = mainBalance - inputValue;
     const addBalance = collectBalance + inputValue;
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <h2 class="text-xl mb-2 font-semibold">${inputValue} Taka is ${titleValue}</h2>
+      <p class="text-black">Date: ${new Date().toLocaleDateString()}, time: ${new Date().toLocaleTimeString()}</p>
+    `;
+    div.classList.add('border', 'p-3' ,'rounded-xl',)
+
+    history.appendChild(div);
     
     document.getElementById('card-balance1').innerText = addBalance;
     document.getElementById('balance').innerText = donateBalance;
@@ -26,11 +56,21 @@ donateBtn2.onclick = ()=>{
 
 const donateBtn3 = document.getElementById('donate-btn3');
 donateBtn3.onclick = ()=>{
+    const history = document.getElementById('history');
     const inputValue = getInputValue('amount-input3');
     const mainBalance = getTextValue('balance');
     const collectBalance = getTextValue('card-balance3');
+    const titleValue = getTitleValue('title3');
     const donateBalance = mainBalance - inputValue;
     const addBalance = collectBalance + inputValue;
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <h2 class="text-xl mb-2 font-semibold">${inputValue} Taka is ${titleValue}</h2>
+      <p class="text-black">Date: ${new Date().toLocaleDateString()}, time: ${new Date().toLocaleTimeString()}</p>
+    `;
+    div.classList.add('border', 'p-3' ,'rounded-xl',)
+    history.appendChild(div);
     
     document.getElementById('card-balance3').innerText = addBalance;
     document.getElementById('balance').innerText = donateBalance;
--- js/utilitise.js
+++ js/utilitise.js
@@ -9,3 +9,8 @@ const textValue = document.getElementById(id).innerText;
 const textConvert = parseFloat(textValue);
 return textConvert;
 }
+
+function getTitleValue(id){
+ const titleValue = document.getElementById(id).innerText;
+ return titleValue;
+}
